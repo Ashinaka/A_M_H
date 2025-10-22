82f615b8b9f963ede99ee376c64d8a426025eb27
Ashinaka <tkl.nkmr.3093@gmail.com>
2025-10-22 14:40:53 +0900

    change command

--- main.py
+++ main.py
@@ -44,27 +44,33 @@ def a_m_h(player_choice):
 
 print("あっちむいてほいをしましょう！")
 while True:
-    player_hand = int(input("あなたの手を入力してください グー:1 チョキ:2 パー:3 \n"))
-    r_p_s_result = r_p_s(player_hand)
-    print()
-    if r_p_s_result == 2:
-        print("じゃんけんに勝ちました！")
-        player_choice = int(input("指をどっちに向けますか？ ↑:1 →:2 ↓:3 ←:4 \n"))
-        a_m_h_result = a_m_h(player_choice)
-        if a_m_h_result == 1:
-            print("あなたの勝ちです！")
-            break
-        else: 
-            print("ハズレです ><")
-    elif r_p_s_result == 1: 
-        print("あいこです！")
-    elif r_p_s_result == 0:
-        print("じゃんけんに負けました ><")
-        player_choice = int(input("頭をどっちに向けますか？ ↑:1 →:2 ↓:3 ←:4 \n"))
-        a_m_h_result = a_m_h(player_choice)
-        if a_m_h_result == 1:
-            print("あなたの負けです！")
-            break
-        else: 
-            print("セーフ！ ")
+    player_hand = int(input("あなたの手を入力してください グー:1 チョキ:2 パー:3 やめる:4\n"))
+    if player_hand == 4:
+        break
+    else:
+        while True:
+            r_p_s_result = r_p_s(player_hand)
+            print()
+            if r_p_s_result == 2:
+                print("じゃんけんに勝ちました！")
+                player_choice = int(input("指をどっちに向けますか？ ↑:1 →:2 ↓:3 ←:4 \n"))
+                a_m_h_result = a_m_h(player_choice)
+                if a_m_h_result == 1:
+                    print("あなたの勝ちです！")
+                    break
+                else: 
+                    print("ハズレです ><")
+                    break
+            elif r_p_s_result == 1: 
+                print("あいこです！")
+            elif r_p_s_result == 0:
+                print("じゃんけんに負けました ><")
+                player_choice = int(input("頭をどっちに向けますか？ ↑:1 →:2 ↓:3 ←:4 \n"))
+                a_m_h_result = a_m_h(player_choice)
+                if a_m_h_result == 1:
+                    print("あなたの負けです！")
+                    break
+                else: 
+                    print("セーフ！ ")
+                    break
     print("")
