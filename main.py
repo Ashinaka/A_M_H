aee1d0b0cd3fcf9bba2d84067dd5d92929f1d6ae
Ashinaka <tkl.nkmr.3093@gmail.com>
2025-10-22 15:53:56 +0900

    change command

--- main.py
+++ main.py
@@ -42,9 +42,9 @@ def a_m_h(player_choice):
     return
 
 
-print("あっちむいてほいをしましょう！")
+print("あっち向いてホイ開始")
 while True:
-    player_hand = int(input("あなたの手を入力してください グー:1 チョキ:2 パー:3 やめる:4\n"))
+    player_hand = int(input("プレイヤーの手を入力 グー:1 チョキ:2 パー:3 やめる:4\n"))
     if player_hand == 4:
         break
     else:
@@ -52,25 +52,25 @@ while True:
             r_p_s_result = r_p_s(player_hand)
             print()
             if r_p_s_result == 2:
-                print("じゃんけんに勝ちました！")
-                player_choice = int(input("指をどっちに向けますか？ ↑:1 →:2 ↓:3 ←:4 \n"))
+                print("じゃんけんに勝利")
+                player_choice = int(input("指の方向を入力 ↑:1 →:2 ↓:3 ←:4 \n"))
                 a_m_h_result = a_m_h(player_choice)
                 if a_m_h_result == 1:
-                    print("あなたの勝ちです！")
+                    print("プレイヤーの勝利")
                     break
                 else: 
-                    print("ハズレです ><")
+                    print("ハズレ")
                     break
             elif r_p_s_result == 1: 
-                print("あいこです！")
+                print("あいこ")
             elif r_p_s_result == 0:
-                print("じゃんけんに負けました ><")
-                player_choice = int(input("頭をどっちに向けますか？ ↑:1 →:2 ↓:3 ←:4 \n"))
+                print("じゃんけんに敗北")
+                player_choice = int(input("頭の方向を入力 ↑:1 →:2 ↓:3 ←:4 \n"))
                 a_m_h_result = a_m_h(player_choice)
                 if a_m_h_result == 1:
-                    print("あなたの負けです！")
+                    print("プレイヤーの敗北")
                     break
                 else: 
-                    print("セーフ！ ")
+                    print("セーフ")
                     break
     print("")
