23dbdacf67df1ef09206bb54777c72574624c654
Ashinaka <tkl.nkmr.3093@gmail.com>
2025-10-27 16:14:29 +0900

    change command

--- main.py
+++ main.py
@@ -45,33 +45,30 @@ def a_m_h(player_choice):
 
 print("あっち向いてホイ開始")
 while True:
-    player_hand = int(input("プレイヤーの手を入力 グー:1 チョキ:2 パー:3 やめる:4\n"))
-    if player_hand == 4:
-        break
-    else:
-        while True:
-            r_p_s_result = r_p_s(player_hand)
-            print()
-            if r_p_s_result == 2:
-                print("じゃんけんに勝利")
-                player_choice = int(input("指の方向を入力 ↑:1 →:2 ↓:3 ←:4 \n"))
-                a_m_h_result = a_m_h(player_choice)
-                if a_m_h_result == 1:
-                    print("プレイヤーの勝利")
-                    break
-                else: 
-                    print("ハズレ")
-                    break
-            elif r_p_s_result == 1: 
-                print("あいこ")
-            elif r_p_s_result == 0:
-                print("じゃんけんに敗北")
-                player_choice = int(input("頭の方向を入力 ↑:1 →:2 ↓:3 ←:4 \n"))
-                a_m_h_result = a_m_h(player_choice)
-                if a_m_h_result == 1:
-                    print("プレイヤーの敗北")
-                    break
-                else: 
-                    print("セーフ")
-                    break
+    player_hand = int(input("プレイヤーの手を入力 グー:1 チョキ:2 パー:3\n"))
+    while True:
+        r_p_s_result = r_p_s(player_hand)
+        print()
+        if r_p_s_result == 2:
+            print("じゃんけんに勝利")
+            player_choice = int(input("指の方向を入力 ↑:1 →:2 ↓:3 ←:4 \n"))
+            a_m_h_result = a_m_h(player_choice)
+            if a_m_h_result == 1:
+                print("プレイヤーの勝利")
+                break
+            else: 
+                print("ハズレ")
+                break
+        elif r_p_s_result == 1: 
+            print("あいこ")
+        elif r_p_s_result == 0:
+            print("じゃんけんに敗北")
+            player_choice = int(input("頭の方向を入力 ↑:1 →:2 ↓:3 ←:4 \n"))
+            a_m_h_result = a_m_h(player_choice)
+            if a_m_h_result == 1:
+                print("プレイヤーの敗北")
+                break
+            else: 
+                print("セーフ")
+                break
     print("")
